0d12414eaa24e243db59eb48eadba69653cf6e7c
Kim Joo Min <jminrang@gmail.com>
2016-09-20 18:02:27 +0900

    json parsing skeleton

--- 00_source_code/json.c
+++ 00_source_code/json.c
@@ -1,11 +1,50 @@
 #include "curl_struct.h"
 #include "json_parsing.h"
 
+void json_parsing_print(json_object *obj)
+{
+    printf("json data\n%s\n", json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
+
+}
+
 void json_parsing (void)
 {
-    json_object *new_obj;
+    struct json_object *all_data;
+    struct json_object *coins, *ethereum, *ethereum_tag;
+    int coins_len, ethereum_len, ethereum_tag_len;
 
-    new_obj = json_tokener_parse(target_data.memory);
-//  all data print
-    printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
+    all_data = json_tokener_parse(target_data.memory);
+
+//    json_parsing_print(all_data);
+
+    coins   = json_object_object_get(all_data, "coins");
+    coins_len  = json_object_array_length(coins);
+
+    struct  coin    ethereum_data;
+
+    ethereum_data.data  = json_object_object_get(coins, "Ethereum");
+    ethereum_data.tag   = json_object_object_get(ethereum_data.data, "tag");
+    ethereum_data.tag_data  = malloc(json_object_get_string_len(ethereum_data.tag));
+    ethereum_data.tag_data  = json_object_get_string(ethereum_data.tag);
+
+    printf("ethereum tag len  = %d\n", json_object_get_string_len(ethereum_data.tag));
+    printf("ethereum tag data = %s\n", ethereum_data.tag_data);
+/*
+    ethereum    = json_object_object_get(coins, "Ethereum");
+    ethereum_len    = json_object_array_length(ethereum);
+
+    ethereum_tag    = json_object_object_get(ethereum, "tag");
+    ethereum_tag_len    = json_object_get_string_len(ethereum_tag);
+
+    const char *string;
+
+    string  = malloc(json_object_get_string_len(ethereum_tag));
+    string = json_object_get_string(ethereum_tag);
+
+    printf("coins len = %d\n", coins_len);
+    printf("ethereum len = %d\n", ethereum_len);
+    printf("ethereum tag len = %d\n", ethereum_tag_len);
+    printf("etheruem tag = %s\n", string);
+    json_parsing_print(ethereum);
+*/
 }
--- 01_include/json_parsing.h
+++ 01_include/json_parsing.h
@@ -1,5 +1,34 @@
 #ifndef __JSON_PARSING_H__
 #define __JSON_PARSING_H__
+/*
+   "tag": "ETH",
+   "algorithm": "Ethash",
+   "block_time": "13.6025",
+   "block_reward": 5.000000,
+   "block_reward24": 5.000000,
+   "last_block": 2293093,
+   "difficulty": 68498882700885.500000,
+   "difficulty24": 63441880821047.898438,
+   "nethash": "5035756860936.261716596213931262635544936592538136",
+   "exchange_rate": 0.022067,
+   "exchange_rate24": 0.021427,
+   "exchange_rate_vol": 14153.215720,
+   "exchange_rate_curr": "BTC",
+   "market_cap": "$1,130,900,929.35",
+   "estimated_rewards": "0.1072",
+   "estimated_rewards24": "0.1158",
+   "btc_revenue": "0.00237",
+   "btc_revenue24": "0.00255",
+   "profitability": 100,
+   "profitability24": 100,
+   "lagging": false,
+   "timestamp": 1474360026
+*/
+struct coin {
+    json_object *data;
+    json_object *tag;
+    const char *tag_data;
+};
 
 void json_parsing (void);
 
